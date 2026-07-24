2da6d1f7fd6942ca166b173e746f1659fac44a6e
Abhishek M B <abhianitha2007@gmail.com>
2026-07-24 17:51:57 +0530

    Add URL submission form

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
 
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    url = request.form["url"]
+
+    return f"""
+
+    <h2>URL Received</h2>
+
+    <p>{url}</p>
+
+    """
+
+
 if __name__ == "__main__":
+
     app.run(debug=True)
