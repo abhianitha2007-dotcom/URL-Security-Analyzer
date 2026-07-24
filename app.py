7cc29b32fc3ef368e8466c81a25a699c0fa666a9
Abhishek M B <abhianitha2007@gmail.com>
2026-07-24 17:37:16 +0530

    Add homepage template and CSS styling

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "<h2>About the Project</h2>"
-
-@app.route("/contact")
-def contact():
-    return "<h2>Contact Page</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True)
